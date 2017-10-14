196b631c907e167fb445bb37401066c787dde64a
optimistex <optimistex@gmail.com>
2017-10-14 06:20:48 +0300

    Add \optimistex\deploy\DeployApplication

--- shell_lib.php
+++ shell_lib.php
@@ -29,12 +29,29 @@ class ShellHelper
             $response = [];
             exec($command . ' 2>&1', $response, $error_code);
             if ($error_code > 0 && empty($response)) {
-                $response = array('Error');
+                $response = array('Error: ' . $error_code);
             }
             $response = implode("\n", $response);
             $res .= "$ $command \n{$response}\n" . ($response ? "\n" : '');
         }
         return $res;
+    }
+
+    /**
+     * Returning an absolute path to "php". It is useful, cause just "php" not working!
+     * @return string
+     */
+    public static function php()
+    {
+        if (defined('PHP_BINDIR') && PHP_BINDIR) {
+            return PHP_BINDIR . '/php';
+        } else if (defined('PHP_BINARY') && PHP_BINARY) {
+            return PHP_BINARY . '/php';
+        } else if (defined('PHP_BINDER') && PHP_BINDER) {
+            return PHP_BINDER . '/php';
+        } else {
+            return 'php';
+        }
     }
 }
 
@@ -72,16 +89,6 @@ class LogHelper
         $datetime = date('Y.m.d H:i:s');
         file_put_contents(static::$log_file, $datetime . "\t" . $message . "\n", FILE_APPEND | LOCK_EX);
         flush();
-    }
-
-    protected static function log_var($name, $var)
-    {
-        static::log($name . ': ' . print_r($var, true));
-    }
-
-    protected static function log_error($message)
-    {
-        static::log('ERROR: ' . $message);
     }
 }
 
@@ -167,5 +174,89 @@ class GitHelper
             }
         }
         return false;
+    }
+}
+
+/**
+ * Main class for deploying
+ * Example for use it:
+ *
+ * ```php
+ * (new DeployApplication('security_key'))->run();
+ * ```
+ *
+ * @package optimistex\deploy
+ */
+class DeployApplication
+{
+    private $securityKey;
+    private $log_file;
+    private $hasAccess;
+
+    function __construct($securityKey, $project_root = '.', $log_file = 'git-deploy-log.txt')
+    {
+        $this->securityKey = $securityKey;
+        $this->log_file = getcwd() . '/' . $log_file;
+        chdir($project_root);
+        LogHelper::init($this->log_file);
+    }
+
+    public function run(array $customCommands = [])
+    {
+        $this->begin();
+        $this->execute($customCommands);
+        $this->end();
+    }
+
+    public function begin()
+    {
+        if ($this->checkSecurity()) {
+            LogHelper::log('SESSION START');
+        }
+    }
+
+    public function execute(array $customCommands = [])
+    {
+        if (!$this->checkSecurity()) {
+            return;
+        }
+        if (empty($customCommands)) {
+            LogHelper::log(ShellHelper::exec([
+                'git branch',
+                'git pull',
+            ]));
+        } else {
+            LogHelper::log(ShellHelper::exec($customCommands));
+        }
+    }
+
+    public function end()
+    {
+        if ($this->checkSecurity()) {
+            LogHelper::log('SESSION END');
+        }
+        if (file_exists($this->log_file)) {
+            echo '<h1>LOG </h1><pre>';
+            echo file_get_contents($this->log_file);
+            echo '</pre>';
+        } else {
+            echo 'log not found';
+        }
+    }
+
+    private function checkSecurity()
+    {
+        if ($this->hasAccess === null) {
+            $this->hasAccess = false;
+            if (isset($_GET['key']) && !empty($_GET['key'])) {
+                if ($this->securityKey === $_GET['key']) {
+                    LogHelper::log('ACCESS IS OBTAINED');
+                    $this->hasAccess = true;
+                } else {
+                    LogHelper::log('DENY << ://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+                }
+            }
+        }
+        return $this->hasAccess;
     }
 }
