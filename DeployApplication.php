c146a1b987d2a8d37efd53b74e1528597102a025
optimistex <optimistex@gmail.com>
2017-10-21 02:58:12 +0300

    implemented breaking execution command on errors

--- DeployApplication.php
+++ DeployApplication.php
@@ -28,6 +28,9 @@ namespace optimistex\deploy;
  */
 class DeployApplication
 {
+    /** @var bool */
+    public $breakOnExecError = true;
+
     /**
      * Private key for protection
      * @var string
@@ -49,6 +52,9 @@ class DeployApplication
     /** @var boolean */
     private $logError = false;
 
+    /** @var bool */
+    private $hasExecError = false;
+
     /**
      * DeployApplication constructor.
      * @param string $securityKey string key for protect application
@@ -66,12 +72,14 @@ class DeployApplication
     /**
      * Fastest executing for typical cases
      * @param array $customCommands
+     * @return bool
      */
     public function run(array $customCommands = [])
     {
         $this->begin();
-        $this->execute($customCommands);
+        $res = $this->execute($customCommands);
         $this->end();
+        return $res;
     }
 
     /**
@@ -87,17 +95,20 @@ class DeployApplication
     /**
      * Executing command like a command lines
      * @param array $customCommands you can execute custom commands
+     * @return bool
      */
     public function execute(array $customCommands = [])
     {
         if (!$this->checkSecurity()) {
-            return;
+            return false;
         }
+        $this->hasExecError = false;
         if (empty($customCommands)) {
             $this->exec(['git branch', 'git pull']); // git pull && git log -1
         } else {
             $this->exec($customCommands);
         }
+        return $this->hasExecError;
     }
 
     /**
@@ -173,9 +184,13 @@ class DeployApplication
                 exec($command . ' 2>&1', $response, $error_code);
                 if ($error_code > 0 && empty($response)) {
                     $response = array('Error: ' . $error_code);
+                    $this->hasExecError = true;
                 }
                 $response = implode("\n", $response);
                 $this->log($response . "\n" . ($response ? "\n" : ''));
+            }
+            if ($this->hasExecError && $this->breakOnExecError) {
+                return;
             }
         }
     }
